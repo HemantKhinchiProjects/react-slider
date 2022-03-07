6ef203e3f783e67418519fd5b4dfd598f837ea94
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-03-07 12:49:23 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import './style.css';
 export default function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index > 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
   return (
     <section className="section">
       <div className="title">
@@ -16,18 +25,18 @@ export default function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
-          let pos = 'nextSlide';
+          let position = 'nextSlide';
           if (personIndex === index) {
-            pos = 'activeSlide';
+            position = 'activeSlide';
           }
           if (
             personIndex === index - 1 ||
             (index === 0 && personIndex === people.length - 1)
           ) {
-            pos = 'lastSlide';
+            position = 'lastSlide';
           }
           return (
-            <article key={id}>
+            <article key={id} className={position}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
